9e6a68f61745800dc06f6194546e752f2da539b9
vaibhav <vaibhavmbankar111@gmail.com>
2022-12-27 16:23:39 +0530

    pk

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "aws" {
+    access_key = "AKIA2RCJY4YNOF6HLJGT"
+    secret_key = "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"
+    region = "us-east-1"
   
 }
 resource "aws_iam_user" "terra" {
   name = "pk"
-  
+
 }
